2ad2e7151c63a730c106ac5ba0fe7fb4c4a7b801
fredb <fred.brosman06@gmail.com>
2024-07-31 16:59:56 +0300

    gsap addded

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box" ref="box"></div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+
+export default {
+  mounted() {
+    gsap.to(this.$refs.box, {duration: 2, x: 100, opacity: 0.5});
+  }
+};
+</script>
+
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
