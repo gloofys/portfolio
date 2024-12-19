dc00bbf13488ae31de95d025551e0354e92f2dc0
fredb <fred.brosman06@gmail.com>
2024-12-19 14:28:18 +0200

    Changed cv link to my new cv.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,7 @@
             <path d="M22.23 0H1.77C.792 0 0 .77 0 1.72v20.56C0 23.23.792 24 1.77 24h20.46c.978 0 1.77-.77 1.77-1.72V1.72C24 .77 23.208 0 22.23 0zM7.16 20.452H3.648V9.036H7.16v11.416zM5.404 7.6c-1.104 0-1.993-.89-1.993-1.986 0-1.095.89-1.985 1.993-1.985s1.992.89 1.992 1.985c0 1.096-.888 1.986-1.992 1.986zM20.452 20.452h-3.516v-5.69c0-1.356-.027-3.098-1.889-3.098-1.892 0-2.181 1.477-2.181 3.003v5.785h-3.516V9.036h3.38v1.556h.049c.47-.888 1.613-1.822 3.318-1.822 3.548 0 4.201 2.334 4.201 5.368v6.314z"/>
           </svg>
         </a>
-        <a href="https://drive.google.com/file/d/13_nzqKP1soc1VWG6XjZrV51PDIjP2vGx/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1aUr9iR7RXMLaJLjzofdVfHDISMH_Kj4T/view?usp=sharing" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="w-4 h-4 sm:w-6 sm:h-6 md:w-8 md:h-8 lg:w-8 lg:h-8 text-customBlue hover:text-blue-800 transition duration-300" viewBox="0 0 24 24">
             <!-- Mask for the CV letters -->
             <mask id="cv-mask">
